9481ce89c9df55fabcf94bc95c79f797f8554a62
Herbert Soares <o_herbert@hotmail.com>
2020-08-26 00:39:28 -0300

    Classe produto e relacionamento, ok

--- src/main/java/com/herbert/cursomc/CursomcApplication.java
+++ src/main/java/com/herbert/cursomc/CursomcApplication.java
@@ -1,13 +1,50 @@
 package com.herbert.cursomc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursomcApplication {
+import com.herbert.cursomc.domain.Categoria;
+import com.herbert.cursomc.domain.Produto;
+import com.herbert.cursomc.repositories.CategoriaRepository;
+import com.herbert.cursomc.repositories.ProdutoRepository;
 
+@SpringBootApplication
+public class CursomcApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	@Autowired
+	private ProdutoRepository produtoRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
+		
+		
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null,"Escritorio");
+		Categoria cat2 = new Categoria(null,"Informatica");
+		
+		Produto prod1 = new Produto("Computador",2000.00);
+		Produto prod2 = new Produto("Impressora",800.00);
+		Produto prod3 = new Produto("Teclado",80.00);
+		
+		cat1.getProdutos().addAll(Arrays.asList(prod1,prod2,prod3));
+		cat2.getProdutos().add(prod2);
+		
+		prod1.getCategorias().add(cat1);
+		prod2.getCategorias().addAll(Arrays.asList(cat1,cat2));
+		prod3.getCategorias().add(cat1);
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
+		produtoRepository.saveAll(Arrays.asList(prod1,prod2,prod3));
 	}
 
 }
--- src/main/java/com/herbert/cursomc/domain/Categoria.java
+++ src/main/java/com/herbert/cursomc/domain/Categoria.java
@@ -1,11 +1,14 @@
 package com.herbert.cursomc.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 
 
 @Entity
@@ -16,6 +19,9 @@ public class Categoria implements Serializable {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
+	
+	@ManyToMany(mappedBy = "categorias")
+	private List<Produto> produtos = new ArrayList<Produto>();
 	
 	public Categoria() {
 	}
@@ -41,6 +47,15 @@ public class Categoria implements Serializable {
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
+	
+
+	public List<Produto> getProdutos() {
+		return produtos;
+	}
+
+	public void setProdutos(List<Produto> produtos) {
+		this.produtos = produtos;
+	}
 
 	@Override
 	public int hashCode() {
@@ -49,6 +64,7 @@ public class Categoria implements Serializable {
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
 		return result;
 	}
+	
 
 	@Override
 	public boolean equals(Object obj) {
